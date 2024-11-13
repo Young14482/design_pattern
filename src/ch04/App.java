7a96bd363b8f7f34f7f46af9e7feb9927138703f
young <young14482@gmail.com>
2024-11-13 10:19:28 +0900

    4. 싱글톤패턴

--- src/ch04/App.java
+++ src/ch04/App.java
@@ -2,7 +2,12 @@ package ch04;
 
 public class App {
     public static void main(String[] args) {
-        Doorman doorman = new Doorman();
+        Doorman doorman = Doorman.instance;
+        Doorman doorman2 = Doorman.instance;
+
+        System.out.println(doorman.hashCode());
+        System.out.println(doorman2.hashCode());
+
         Mouse mouse = new Mouse();
         Cat cat = new Cat();
 
